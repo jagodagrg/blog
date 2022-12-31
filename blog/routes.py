624910bf5db7a837061f112098d36734666ba22b
Jagoda Giurg <jagoda.giurg@gmail.com>
2022-12-31 11:04:15 +0000

    Add logging in functionality and drafts

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,17 @@
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
-from flask import render_template, request, flash, redirect, url_for
+from blog.forms import EntryForm, LoginForm
+from flask import render_template, request, flash, redirect, url_for, session
+import functools
+
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get("logged_in"):
+            return view_func(*args, **kwargs)
+        return redirect(url_for("login", next=request.path))
+    return check_permissions
 
 
 @app.route("/")
@@ -13,9 +23,10 @@ def index():
 
 @app.route("/posts/<int:entry_id>", methods=["GET", "POST"])
 @app.route("/posts/", methods=["GET", "POST"])
+@login_required
 def create_or_edit_entry(entry_id=None):
     errors = None
-    if entry_id != None:
+    if entry_id is not None:
         entry = Entry.query.filter_by(id=entry_id).first_or_404()
         form = EntryForm(obj=entry)
         if request.method == "POST":
@@ -40,3 +51,47 @@ def create_or_edit_entry(entry_id=None):
             else:
                 errors = form.errors
     return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/posts/<int:entry_id>/delete", methods=["POST"])
+@login_required
+def delete_entry(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    errors = None
+    if request.method == "POST":
+        db.session.delete(entry)
+        db.session.commit()
+        flash('Wpis usunięty!')
+    return redirect(url_for("index"))
+
+
+@ app.route("/login/", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get("next")
+    if request.method == "POST":
+        if form.validate_on_submit():
+            session["logged_in"] = True
+            session.permanent = True  # Use cookie to store session.
+            flash("You are now logged in.", "success")
+            return redirect(next_url or url_for("index"))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@ app.route("/logout/", methods=["GET", "POST"])
+def logout():
+    if request.method == "POST":
+        session.clear()
+        flash("You are now logged out.", "success")
+    return redirect(url_for("index"))
+
+
+@ app.route("/drafts/", methods=['GET'])
+@ login_required
+def list_drafts():
+    drafts = Entry.query.filter_by(
+        is_published=False).order_by(Entry.pub_date.desc())
+    return render_template("drafts.html", drafts=drafts)
